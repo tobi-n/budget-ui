a66bb4795b2e8a5cef02d362dc4d07a088b765b6
Tobi <tobias.nueesch@lernen.siw.swiss>
2023-11-11 09:11:00 +0100

    2023-10-31 Kategorie bearbeiten & löschen

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -36,7 +36,10 @@ export class CategoryListComponent {
 
 
   async openModal(category?: Category): Promise<void> {
-    const modal = await this.modalCtrl.create({ component: CategoryModalComponent });
+    const modal = await this.modalCtrl.create({
+      component: CategoryModalComponent,
+      componentProps: { category: category ? { ...category } : {} },
+    });
     modal.present();
     const { role } = await modal.onWillDismiss();
     if (role === 'refresh') this.reloadCategories();
